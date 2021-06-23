e2e0f9e531eb33ba76e13da8c15e01111fd18c9b
tobiaszimmermann <cph-tz11@cphbusiness.dk>
2021-06-23 21:17:03 +0200

    feat: get is available from useRouteData

--- src/app/use-route-data.tsx
+++ src/app/use-route-data.tsx
@@ -1,15 +1,32 @@
+import type * as Muxa from "../types";
 import { useEffect, useState } from "react";
-import { Get } from "../types";
+import { useParams } from "react-router-dom";
 import { useRouterContext } from "./ssr-router";
 
 type RouteData<Data> = {
   data: Data | undefined;
-  get: Get;
+  get: () => Promise<unknown>;
 };
 
 export default function useRouteData<Data>(path?: string): RouteData<Data> {
-  let { routes } = useRouterContext();
+  let { routes, dispatch } = useRouterContext();
   let [routeData, setRouteData] = useState<RouteData<Data>>();
+  let params = useParams();
+
+  function getLoader(route: Muxa.RouteData): () => Promise<unknown> {
+    return async () => {
+      try {
+        let data = await route.get(params);
+        dispatch({
+          type: "ADD_ROUTE_DATA",
+          path: route.path,
+          routeData: data,
+        });
+      } catch (err) {
+        console.error(err.message);
+      }
+    };
+  }
 
   useEffect(() => {
     let location = window.location.pathname;
@@ -20,14 +37,15 @@ export default function useRouteData<Data>(path?: string): RouteData<Data> {
       return route.path === location;
     });
     if (!route) return;
+    let get = getLoader(route);
     setRouteData({
       data: route.routeData as Data | undefined,
-      get: route.get,
+      get,
     });
   }, []);
 
   return {
     data: routeData?.data,
-    get: routeData?.get as Get,
+    get: routeData?.get as () => Promise<unknown>,
   };
 }
